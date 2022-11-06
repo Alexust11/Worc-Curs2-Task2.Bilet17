0ab229b2d411dbec0fbce43b34c869e30c52b02b
User <alexust11@yandex.ru>
2022-11-06 21:33:58 +0700

    Задача 2. Билет 17

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("1", 5);
+        map.put("2", 6);
+        map.put("3", 7);
+        map.put("4", 8);
+        map.put("5", 9);
+        Integer s = 0;
+        for (Integer n : map.values()) {
+            s = s + n;
+        }
+        System.out.println(s);
+        s = 0;
+        Iterator<Map.Entry<String, Integer>> it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, Integer> entry = it.next();
 
-        System.out.println("Hello world!");
+            s = s + entry.getValue();
+
+
+        }
+        System.out.println(s);
+
     }
 }
